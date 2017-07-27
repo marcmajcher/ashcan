1579feef83d5e5361c83cdaa75055b8b475be24c
Marc Majcher <majcher@gmail.com>
2017-07-27 10:14:43 -0500

    more setup

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # ashcan
 
 ## TODO
+
+hook up knex and pg
+make simple user and game models
+Save user when they log in with google
+
+Views:
+    not logged in
+
+    logged in
+        user page
+
+Models:
+    User
+    Game
+        Version
+
+    Playtest
+
+
+Flag yourself as play tester, prototyper, publisher, etc
+Tag yourself with location, interests, etc
+Upload playtest materials (paid) or link to them
+Versions of playtest materials
+Sign up for playtests
+Publish a new playtest
+Get feedback - forms
+Some kind of sweet currency for stuff
+search for games / testers / etc
+profile pages
+not logged in - feed of stuff coming up
+
+Set up org for google auth and email?
--- app/app.js
+++ app/app.js
@@ -2,21 +2,28 @@
 
 /* eslint-env node */
 
+require('dotenv').load();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('cookie-session');
+const randomString = require('randomstring');
+const methodOverride = require('method-override');
 
 const index = require('./routes/index');
 const api = require('./routes/api');
 const app = express();
+app.disable('x-powered-by');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
+app.use(methodOverride('_method'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -24,6 +31,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
+
+const numKeys = Math.ceil(Math.random() * 10) + 10; // eslint-disable-line no-magic-numbers
+const randomKeys = [];
+for (let i = 0; i < numKeys; i++) {
+  randomKeys.push(randomString.generate());
+}
+const sessionMiddleware = session({
+  keys: randomKeys
+});
+app.use(sessionMiddleware);
 
 app.use('/', index);
 app.use('/api', api);
